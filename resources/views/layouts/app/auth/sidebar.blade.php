2f75f716091022cbb63988d722d6734a1a46384f
Patryk Wychowaniec <wychowaniec.patryk@gmail.com>
2017-07-15 23:45:39 +0200

    Mark current section as selected in the sidebar.

--- resources/views/layouts/app/auth/sidebar.blade.php
+++ resources/views/layouts/app/auth/sidebar.blade.php
@@ -1,6 +1,7 @@
 @php
     /**
      * @var string[] $sectionNames
+     * @var string $sectionName
      * @var \App\ValueObjects\Sidebar $sidebar
      */
 @endphp
@@ -9,16 +10,16 @@
     <div id="sidebar">
         {{-- Section list --}}
         <select id="section-select" class="form-control">
-            @foreach ($sectionNames as $sectionName)
-                <option value="{{ $sectionName }}">
-                    {{ __(sprintf('common/sections.%s.name', $sectionName)) }}
+            @foreach ($sectionNames as $itSectionName)
+                <option value="{{ $itSectionName }}" {{ $sectionName === $itSectionName ? 'selected' : '' }}>
+                    {{ __(sprintf('common/sections.%s.name', $itSectionName)) }}
                 </option>
             @endforeach
         </select>
 
         <hr>
 
-        {{-- Current section's sidebar links --}}
+        {{-- Current section's sidebar --}}
         <div class="panel-group" id="sidebar-menu">
             @each('layouts.app.auth.sidebar.items', $sidebar->getItems(), 'sidebarItem')
         </div>
